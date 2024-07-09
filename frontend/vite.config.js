39718c42fb023ed1633fe5f06d2c9d9b999a2968
santhosh-mce <santhosh.mce1234@gmail.com>
2024-07-09 12:35:04 +0530

    first commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://blogs-page-2tjg.onrender.com',
         secure: false,
       },
     },
